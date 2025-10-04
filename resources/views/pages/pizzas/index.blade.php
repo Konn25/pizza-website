a1ce12ba5a3d6c1073c740e29bf5f37276e46c54
Konn25 <koncz.bence9@gmail.com>
2025-10-04 21:42:50 +0200

    Add edit and delete button

--- resources/views/pages/pizzas/index.blade.php
+++ resources/views/pages/pizzas/index.blade.php
@@ -3,37 +3,44 @@
 @section('title', 'Pizzas')
 
 @section('breadcrumb')
-    <div class="row mb-2">
-        <div class="col-sm-6">
-            <h1 class="m-0">Pizzas</h1>
-        </div>
-        <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item active">
-                    Pizzas
-                </li>
-            </ol>
-        </div>
+<div class="row mb-2">
+    <div class="col-sm-6">
+        <h1 class="m-0">Pizzas</h1>
     </div>
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item active">
+                Pizzas
+            </li>
+        </ol>
+    </div>
+</div>
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col">
-            @foreach ($pizzas as $pizza)
-                <div class="card card-outline card-primary">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ $pizza->name }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <img src="{{ $pizza->url }}" alt="pizza" width="250" height="250">
-                        <br>
-                        {{ $pizza->description }}
-                    </div>
-                </div>
-            @endforeach
+<div class="row">
+    @foreach ($pizzas as $pizza)
+    <div class="col-3">
+        
+        <div class="card card-outline card-primary">
+            <div class="card-header">
+                <h3 class="card-title">{{$pizza->name}}</h3>
+            </div>
+            <div class="card-body">
+                <img src="{{$pizza->url}}" alt="pizza" width="250" height="250">
+                <br>
+                <br>
+                {{$pizza->description}}
+                
+            </div>
+            <div class="card-footer d-flex justify-content-center" style="gap: 10px;">
+                <a href="{{route('pizzas.edit',$pizza)}}"><button type="button" class="btn btn-outline-primary"><i class="fas fa-pen"></i></button></a>
 
+                <button type="submit" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+            </div>
         </div>
-
     </div>
+    @endforeach
+
+</div>
 @endsection
