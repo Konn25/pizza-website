81c0f8b6aeca80d2702c2f66ef8fb0053dc45682
Konn25 <koncz.bence9@gmail.com>
2025-10-19 18:08:22 +0200

    Change edit page frontend

--- resources/views/pages/users/edit.blade.php
+++ resources/views/pages/users/edit.blade.php
@@ -1,36 +1,120 @@
 @extends('layouts.app')
 
-@section('title', $user->name)
+@section('title', 'Edit ' . $user->name)
 
 @section('breadcrumb')
 <div class="row mb-2">
     <div class="col-sm-6">
-        <h1 class="m-0">{{$user->name}}</h1>
+        <h1 class="m-0">Edit User: {{$user->name}}</h1>
     </div>
     <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item">
-                <a href="{{route('users.index')}}">Users</a>
+                <a href="{{ route('users.index') }}">Users</a>
             </li>
-            <li class="breadcrumb-item active">
-                {{$user->name}}
+            <li class="breadcrumb-item">
+                <a href="{{ route('users.show', $user) }}">{{$user->name}}</a>
             </li>
+            <li class="breadcrumb-item active">Edit</li>
         </ol>
     </div>
 </div>
 @endsection
 
+@if (session('success'))
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Error!</strong> Please fix this error(s):
+        <ul class="mb-0 mt-2">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
 @section('content')
 <div class="row">
-    <div class="col">
+    <div class="col-md-8">
         <div class="card card-primary card-outline">
+            <div class="card-header">
+                <h3 class="card-title">Edit User Information</h3>
+            </div>
             <div class="card-body">
-                Name: {{$user->name}} <br>
-                Email: {{$user->email}} <br>
-                Phone: {{$user->phone}}
+                <form action="{{ route('users.update', $user) }}" method="POST">
+                    @csrf
+                    @method('PUT')
+
+                    <div class="form-group mb-3">
+                        <label for="name">Name</label>
+                        <input 
+                            type="text" 
+                            name="name" 
+                            id="name" 
+                            class="form-control @error('name') is-invalid @enderror"
+                            value="{{ old('name', $user->name) }}"
+                            required
+                        >
+                        @error('name')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="email">Email</label>
+                        <input 
+                            type="email" 
+                            name="email" 
+                            id="email" 
+                            class="form-control @error('email') is-invalid @enderror"
+                            value="{{ old('email', $user->email) }}"
+                            required
+                        >
+                        @error('email')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="phone">Phone</label>
+                        <input 
+                            type="text" 
+                            name="phone" 
+                            id="phone" 
+                            class="form-control @error('phone') is-invalid @enderror"
+                            value="{{ old('phone', $user->phone) }}"
+                        >
+                        @error('phone')
+                            <div class="invalid-feedback">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <div class="d-flex justify-content-between">
+                        <a href="{{ route('users.show', $user) }}" class="btn btn-secondary">
+                            Cancel
+                        </a>
+                        <button type="submit" class="btn btn-primary">
+                            Save Changes
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-
 </div>
 @endsection
+
+<script>
+    setTimeout(() => {
+        document.querySelectorAll('.alert').forEach(el => el.remove());
+    }, 4000);
+</script>
